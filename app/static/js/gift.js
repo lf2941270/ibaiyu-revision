c68479898e455948f0f6e2edd8ff7ff1ecd984af
李凡 <lf2941270@163.com>
2015-02-14 18:14:35 +0800

    轮播和最新活动

--- app/static/js/gift.js
+++ app/static/js/gift.js
@@ -1,12 +1,36 @@
 (function(){
-	/*function show_img_title() {
-		$('body').delegate('.post-thumbnail', 'mouseover', function(){
-			$(this).find(".img_title").slideDown('fast');
-		}).delegate('.post-thumbnail', 'mouseout', function(){
-					$(this).find(".img_title").slideUp();
-				});
+	//幻灯片
+	function slider_action()	{
+		if( $('.banner').length>0)
+		{
+			var unslider = $('.banner').unslider({
+				speed: 500,               //  滚动速度
+				delay: 5000,              //  动画延迟
+//				complete: function() {$('.banner_info').animate({bottom:"-80px"},"fast");},  //  动画完成的回调函数
+				keys: true,               //  启动键盘导航
+				dots: false,               //  显示点导航
+				fluid: false              //  支持响应式设计
+			});
+			$('.unslider-arrow').click(function() {
+				var fn = this.className.split(' ')[1];
+				//  Either do unslider.data('unslider').next() or .prev() depending on the className
+				$('.banner_info').animate({bottom:"-80px"},"fast",function(){unslider.data('unslider')[fn]();});
+			});
+
+			$('.banner>ul>li').hover(
+				function() {
+//					$('.unslider-arrow').css("display","block");
+					$('.banner_info').stop().animate({bottom:"0px"},"fast");
+				},
+				function()	{
+//					$('.unslider-arrow').css("display","none");
+					$('.banner_info').stop().animate({bottom:"-80px"},"fast");
+				}
+			);
+		}
 	}
+
 	$(function(){
-		show_img_title();
-	})*/
+		slider_action();
+	})
 })();
